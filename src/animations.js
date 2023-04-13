e1cbb90c6f394311c86fed225f79a7a687d09539
qp0kycHuk <serega990112@mail.ru>
2023-04-13 17:56:58 +0300

    add cards animations

--- src/animations.js
+++ src/animations.js
@@ -69,6 +69,57 @@ function loadHandler() {
             }
         )
     }
+    {
+        // feedback 
+        gsap.from(
+            '.feedback__content',
+            {
+                xPercent: -50,
+                scrollTrigger: {
+                    trigger: '.feedback',
+                    start: 'top bottom',
+                    end: 'center 80%',
+                    scrub: 3
+                }
+            }
+        )
+        gsap.from(
+            '.feedback__pack',
+            {
+                xPercent: 20,
+                scrollTrigger: {
+                    trigger: '.feedback',
+                    start: 'top bottom',
+                    end: 'center 80%',
+                    markers: true,
+                    scrub: 3
+                }
+            }
+        )
+    }
+    {
+        // feedback 
+        const cards = gsap.utils.toArray('.advantages__card')
+        cards.forEach((card, index) => {
+            gsap.from(
+                card,
+                {
+                    trigger: card,
+                    yPercent: 50,
+                    opacity: 0,
+                    duration: 1,
+                    delay: index / 10,
+                    scrollTrigger: {
+                        trigger: card,
+                        start: 'top 95%',
+                        markers: true
+                    }
+                }
+            )
+        })
+
+
+    }
 }
 
 
